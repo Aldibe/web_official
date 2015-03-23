39db29cff246564f438b34d5e4703124ffefa0dd
Aldibe <aldi3594@gmail.com>
2015-03-23 17:59:32 +0700

    Config awal (config.php, database.php, routes.php, c_book.php)

---
+++ application/controllers/c_book.php
@@ -0,0 +1,14 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class c_book extends CI_Controller {
+
+
+	public function index()
+	{
+		$this->load->view('home.html');
+	}
+}
+
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
