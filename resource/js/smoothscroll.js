793bb4a194eb5fe666fbd4c3b49106423f99735b
Aldibe <aldi3594@gmail.com>
2015-03-27 16:33:19 +0700

    Fix smooth scrolling

--- resource/js/smoothscroll.js
+++ resource/js/smoothscroll.js
@@ -1,20 +1,16 @@
 if (!window['jQuery']) alert('The jQuery library must be included before the smoothscroll.js file.  The plugin will not work propery.');
 
-var hashTagActive = "";
-    $(".smoothScroll").click(function (event) {
-        if(hashTagActive != this.hash) { //this will prevent if the user click several times the same link to freeze the scroll.
-            event.preventDefault();
-            //calculate destination place
-            var dest = 0;
-            if ($(this.hash).offset().top > $(document).height() - $(window).height()) {
-                dest = $(document).height() - $(window).height();
-            } else {
-                dest = $(this.hash).offset().top - 120;
-            }
-            //go to destination
-            $('html,body').animate({
-                scrollTop: dest
-            }, 2000, 'swing');
-            hashTagActive = this.hash;
-        }
-    });
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top - 120
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
